74d1473aa997d638666b4889e80ccd92094f5d8a
akshay dhangar <akshaygdhangar14@gmail.com>
2022-05-29 16:49:51 +0530

    UC_3

---
+++ UC3_FullPartTIme.java
@@ -0,0 +1,20 @@
+package day4empoywage;
+
+public class UC3_FullPartTIme {
+
+	public static void main(String[] args) {
+		 int fullTimeworker=1;
+         int wagePerHr=20;
+         int emphr=0;
+		int empwage=0;
+	double	empCheck = (int) Math.floor(Math.random() * 10) % 2;
+			if (empCheck ==fullTimeworker )
+				emphr=8;
+			else if(empCheck ==fullTimeworker )
+				emphr=4;
+			empwage=emphr*wagePerHr;
+    System.out.println("EMP Wages of is :"+ empwage);
+
+	}
+
+}
